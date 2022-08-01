cbe2079ef550caa21476671e6edc71412b5aaf51
william <williamchan_7@163.com>
2022-08-01 22:59:16 +0800

    first

---
+++ Assets/Scripts/AndrewScripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //资源
+    public GameObject blood; //血迹
+    public GameObject bloodParticle;//溅血特效
+    public AudioClip bonus;//奖励音效
+
+    //数据
+    public bool isMoving;
+    public float moveAngle;
+    public int speed;
+    public int maxHP;
+    public float HP;
+    public int regenHpSpeed;
+    public int delayRegen;//受到伤害再次回复血量的延迟时间
+    public float delayTimer;//可回复血量计时器 
+
+    //引用
+    private Rigidbody2D playRig;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        GameManager.Instance.player = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 游戏管理
+/// </summary>
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    [HideInInspector]
+    //玩家数据
+    public int money;
+    [HideInInspector]
+    public int unlockedLevel;
+    public int selectLevel;
+    public int gunLevel;
+    public Player player;
+
+    //系统设置
+    public float volume;
+    public float joystickSize;
+
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void LoadGame()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+        {
+            money = PlayerPrefs.GetInt("Money");
+        }
+        else
+        {
+            money = 0;
+        }
+        if (PlayerPrefs.HasKey("Levels"))
+        {
+            unlockedLevel = PlayerPrefs.GetInt("Levels");
+        }
+        else
+        {
+            unlockedLevel = 1;
+        }
+        if (PlayerPrefs.HasKey("Sound"))
+        {
+            volume = PlayerPrefs.GetFloat("Sound");
+        }
+        else
+        {
+            volume = 1;
+        }
+        if (PlayerPrefs.HasKey("Joystick"))
+        {
+            joystickSize = PlayerPrefs.GetFloat("Joystick");
+        }
+        else
+        {
+            joystickSize = 0.3f;
+        }
+        selectLevel = 1;
+    }
+    
+    
+}
